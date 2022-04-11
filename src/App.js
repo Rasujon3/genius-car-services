a8573efabc9fc5874d2d68e02b3711161bfafd69
Rasujon3 <rasujon3@gmail.com>
2022-04-11 10:39:20 +0600

    60-4 Load fake data map services to display single services

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
-import Footer from "./Pages/Shared/Footer/Footer";
+import Home from "./Pages/Home/Home/Home";
 import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        <Route path="/"></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
