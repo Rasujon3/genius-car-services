d833b3cbbfb2ba67c1fa7672cc0267d7b80a4232
Rasujon3 <rasujon3@gmail.com>
2022-04-12 23:36:06 +0600

    61-6 set environment variable form firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbII7V-ItdkYHMil3JgsIS_QhKovAoLpA",
-  authDomain: "genius-car-services-6b155.firebaseapp.com",
-  projectId: "genius-car-services-6b155",
-  storageBucket: "genius-car-services-6b155.appspot.com",
-  messagingSenderId: "742216187906",
-  appId: "1:742216187906:web:681b6712af21444afb671b",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
