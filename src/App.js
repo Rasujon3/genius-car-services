c377ce485e1b89814a1ca19ae24e435d0341fb66
Rasujon3 <rasujon3@gmail.com>
2022-04-12 23:12:48 +0600

    61-5 Create Register form and style it

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./Pages/Shared/Footer/Footer";
 import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
 import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/service/:serviceId" element={<ServiceDetail />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer />
