4ea9236372d89f92d2c2606fc23346b4e3080b34
Rasujon3 <rasujon3@gmail.com>
2022-04-19 02:16:23 +0600

    63_5-2 Set Dynamic page title based on Route using react helmet async

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </HelmetProvider>
 );
 
 reportWebVitals();
