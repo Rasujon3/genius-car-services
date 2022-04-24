601f16f1275f69442b7b1dd2ddc1b6fb142d789d
Rasujon3 <rasujon3@gmail.com>
2022-04-24 23:39:53 +0600

    68-3 Get Order data, create API endpoint and readonly input

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useServiceDetail from "../../../hooks/useServiceDetail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const CheckOut = () => {
   const { serviceId } = useParams();
   const [service] = useServiceDetail(serviceId);
-  const [user, setUser] = useState({
+  const [user] = useAuthState(auth);
+
+  /*   const [user, setUser] = useState({
     name: "Akbar The Great",
     email: "akbar@momo.taj",
     address: "Tajmohol Road Md.pur",
@@ -20,16 +24,29 @@ const CheckOut = () => {
     const newUser = { address: newAddress, ...rest };
     console.log(newUser);
     setUser(newUser);
+  }; */
+
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const order = {
+      email: user.email,
+      service: service.name,
+      serviceId: serviceId,
+      address: event.targer.address.value,
+      phone: event.targer.phone.value,
+    };
   };
 
   return (
     <div className="w-50 mx-auto">
       <h2>Please Order: {service.name}</h2>
-      <form>
+      <form onSubmit={handlePlaceOrder}>
         <input
           className="w-100 mb-2"
           type="text"
-          value={user.name}
+          value={user.displayName}
+          readOnly
+          disabled
           name="name"
           placeholder="Name"
           required
@@ -39,6 +56,8 @@ const CheckOut = () => {
           className="w-100 mb-2"
           type="email"
           value={user.email}
+          readOnly
+          disabled
           name="email"
           placeholder="Email"
           required
@@ -57,8 +76,7 @@ const CheckOut = () => {
           className="w-100 mb-2"
           type="text"
           name="address"
-          value={user.address}
-          onChange={handleAddressChange}
+          autoComplete="off"
           placeholder="address"
           required
         />
@@ -67,7 +85,6 @@ const CheckOut = () => {
           className="w-100 mb-2"
           type="text"
           name="phone"
-          value={user.phone}
           placeholder="phone"
           required
         />
